86bdc9e181050c2cc99c794312b30b51e688e47e
Fiqri Sharel <fiqrisharel56@gmail.com>
2026-06-02 11:22:17 +0800

    Update test_gemini.py

--- test_gemini.py
+++ test_gemini.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AQ.Ab8RN6KoDteaPWPr-YtfrTxsqWHandURxPfebl_CAYVFWw3PrQ")
+genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel("gemini-2.5-flash")
 response = model.generate_content("Say hi in one sentence")
 print(response.text)
